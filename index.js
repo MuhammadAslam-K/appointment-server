27f8e4aabf4298f93298c180a3f63fd56776b6d5
MuhammadAslam-K <aslamka.2k3@gmail.com>
2025-03-10 12:12:00 +0530

    update : added port to env

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import { mongoDB } from './config/mongoDB.js'
 import router from './router/index.js'
+dotenv.config();
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(cors('*'))
 app.use(express.json())
